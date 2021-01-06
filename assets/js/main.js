907c090f00fd8214d95342eaa62a10518ed9eb2e
ferhatchala <ferhatchala2016@gmail.com>
2021-01-06 13:18:52 +0100

    set up registration form and animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,22 +33,9 @@ const sr = ScrollReveal({
     duration:2000,
     reset:true
 })
-/*SCROLL HOME */
+/*SCROLL*/
 sr.reveal('.home__title',{})
 sr.reveal('.input-field',{})
 sr.reveal('.button',{delay:200})
 sr.reveal('.home__img',{delay:400})
 sr.reveal('.home__social-icon',{interval:200})
-/*SCROLL ABOUT */
-sr.reveal('.about__img',{})
-sr.reveal('.about__subtitle',{delay:200})
-sr.reveal('.about__text',{delay:400})
-/*SCROLL SKILLS */
-sr.reveal('.skills__subtitle',{})
-sr.reveal('.skills__text',{delay:200})
-sr.reveal('.skills__data',{interval:200})
-sr.reveal('.skills__img',{delay:400})
-/*SCROLL WORK */
-sr.reveal('.work__img',{interval:200})
-/*SCROLL CONTACT */
-sr.reveal('.contact__input',{interval:200})
